a925a668dfb8a9ac33fb2fb398d49e252af902f9
bhawanthas <bpsshanuka@gmail.com>
2022-04-01 07:48:55 +0530

    remove the unwanted comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComposerComponent } from './components/views/composer/composer.component';
@@ -141,7 +140,6 @@ const appRoutes: Routes = [];
     FlexLayoutModule,
     HttpClientModule,
     MatStepperModule,
-    //AceEditorModule,
     RouterModule.forRoot(appRoutes),
     StoreModule.forRoot({ nft: nftReducer, user: userReducer }),
   ],
