591f3507f805725ca82de7becbab5474776f4758
Ishini Kiridena <ishini.kiridenaedu@gmail.com>
2022-04-12 09:45:04 +0530

    TRAC-4355 - SVG code is generated no image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/views/login/login.component';
 import { NftHtmlComponent } from './components/views/nft-html/nft-html.component';
 import { AuthGuard } from './guards/auth.guard';
+import { NftSvgComponent } from './components/views/nft-svg/nft-svg.component';
 
 const routes: Routes = [
   {
@@ -26,6 +27,10 @@ const routes: Routes = [
     redirectTo: '/login',
     pathMatch: 'full',
   },
+  {
+    path: 'nft-svg',
+    component: NftSvgComponent,
+  }
 ];
 
 @NgModule({
--- src/app/components/views/nft-svg/nft-svg.component.ts
+++ src/app/components/views/nft-svg/nft-svg.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { SidenavService } from 'src/app/services/sidenav.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AppState } from 'src/app/store/app.state';
+import { NFTContent } from 'src/models/nft-content/nft.content';
+import { Store } from '@ngrx/store';
+import { ComposerBackendService } from 'src/app/services/composer-backend.service';
+import { selectNFTContent } from 'src/app/store/nft-state-store/nft.selector';
+import domtoimage from 'dom-to-image';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nft-svg',
@@ -8,11 +14,77 @@ import { SidenavService } from 'src/app/services/sidenav.service';
 })
 export class NftSvgComponent implements OnInit {
   sidenav: boolean = true;
-  constructor(private sidebarService: SidenavService) {
-    this.sidebarService.getStatus().subscribe((val) => {
-      this.sidenav = val;
+  svgSrc: any;
+  nft$: NFTContent;
+  nftContent: NFTContent;
+  imageInfo: any[];
+  svg:SafeHtml;
+  htmlStr: any;
+
+  @ViewChild('iframe', { static: false }) iframe: ElementRef;
+
+  constructor(
+    private _composerService: ComposerBackendService,
+    private store: Store<AppState>,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.getNftContent();
+    this.genereateImage();
+  }
+
+  public ngAfterViewInit() {
+    this.populateIframe(this.iframe.nativeElement);
+  }
+
+  private getNftContent() {
+    this.store.select(selectNFTContent).subscribe((data) => {
+      this.nftContent = data;
     });
   }
 
-  ngOnInit(): void {}
+  public genereateImage() {
+    this.convertToSvg().then((data) => {
+      this.svgSrc = data;
+      //this.svg = this.sanitizer.bypassSecurityTrustHtml(this.svgSrc);
+      console.log(this.svgSrc);
+    });
+  }
+
+  public convertToSvg() {
+    function filter(node: any) {
+      return node.tagName !== 'i';
+    }
+
+    var promise = new Promise((resolve, reject) => {
+      var node: any = document.getElementById('image-section');
+      domtoimage
+        .toSvg(node, { filter: filter })
+        .then(function (dataUrl) {
+          var img = new Image();
+          img.src = dataUrl;
+          resolve(img.src);
+        })
+        .catch(function (error) {
+          console.error('Issue', error);
+        });
+    });
+    return promise;
+  }
+
+  private populateIframe(iframe: any) {
+    //get from backend
+    this._composerService
+      .generateHTML(this.nftContent)
+      .subscribe((data:any)=> {
+        if(!!data && !!data.Response && data.Response !== ''){
+          this.htmlStr = atob(data.Response);
+          const content = this.htmlStr;
+          iframe.contentWindow.document.open();
+          iframe.contentWindow.document.write(content);
+          iframe.contentWindow.document.close();
+        }
+      });
+  }
 }
