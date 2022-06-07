ee1c8c6a7885fd827971f6cef3ecc548f57aff64
anjulasjay99 <anjulasjay@gmail.com>
2022-06-07 13:02:18 +0530

    TRAC-4960 fix the issue with confirmation popup

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { JwtserviceService } from 'src/app/services/jwtservice.service';
@@ -6,10 +7,15 @@ import { SidenavService } from 'src/app/services/sidenav.service';
 import { UserserviceService } from 'src/app/services/userservice.service';
 import { AppState } from 'src/app/store/app.state';
 import {
+  selectNFTContent,
+  selectProjectSavedState,
+} from 'src/app/store/nft-state-store/nft.selector';
+import {
   selectUserDetail,
   selectUserName,
 } from 'src/app/store/user-state-store/user.selector';
 import { ComposerUser } from 'src/models/user';
+import { CloseProjectComponent } from '../../modals/close-project/close-project.component';
 
 @Component({
   selector: 'app-header',
@@ -19,24 +25,46 @@ import { ComposerUser } from 'src/models/user';
 export class HeaderComponent implements OnInit {
   public routerProjectLink: string;
   user: ComposerUser;
+  projectSaved: boolean;
   constructor(
+    private store: Store<AppState>,
     private router: Router,
     private sidenav: SidenavService,
-    private userServices:UserserviceService,
-    private jwtServices:JwtserviceService
+    private userServices: UserserviceService,
+    private jwtServices: JwtserviceService,
+    public dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
-    this.user=this.userServices.getCurrentUser()
+    this.user = this.userServices.getCurrentUser();
     this.setLink();
+    this.store.select(selectProjectSavedState).subscribe((status) => {
+      this.projectSaved = status;
+    });
   }
 
   /**
    * @function logout - when click on logout go back to login page and release the token
    */
   public logout() {
-    this.router.navigate(['/login']);
-    sessionStorage.setItem('Token', '');
+    let tenentId = JSON.parse(sessionStorage.getItem('User') || '').TenentId!;
+
+    if (this.router.url !== `/layout/projects/${tenentId}`) {
+      if (!this.projectSaved) {
+        this.dialog.open(CloseProjectComponent, {
+          data: {
+            user: this.user,
+            logout: true,
+          },
+        });
+      } else {
+        this.router.navigate(['/login']);
+        sessionStorage.setItem('Token', '');
+      }
+    } else {
+      this.router.navigate(['/login']);
+      sessionStorage.setItem('Token', '');
+    }
   }
 
   /**
@@ -62,8 +90,26 @@ export class HeaderComponent implements OnInit {
   /**
    * @function setLink - set the project view link
    */
-  public setLink(){
+  public setLink() {
     let tenentId = JSON.parse(sessionStorage.getItem('User') || '').TenentId!;
     this.routerProjectLink = `/layout/projects/${tenentId}`;
+  }
+
+  public goToProjects() {
+    let tenentId = JSON.parse(sessionStorage.getItem('User') || '').TenentId!;
+
+    if (this.router.url !== `/layout/projects/${tenentId}`) {
+      if (!this.projectSaved) {
+        this.dialog.open(CloseProjectComponent, {
+          data: {
+            user: this.user,
+          },
+        });
+      } else {
+        this.router.navigate(['/layout/projects/' + this.user.TenentId]);
+      }
+    } else {
+      this.router.navigate(['/layout/projects/' + this.user.TenentId]);
+    }
   }
 }
